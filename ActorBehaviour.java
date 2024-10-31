7ca4e2cf8296bb0f9a9575ff6b8e5f836aeb91f9
Svetlana <Svetlanat1005@yandex.ru>
2024-10-31 13:20:42 +0300

    add ActorBehaviour.java

---
+++ ActorBehaviour.java
@@ -0,0 +1,10 @@
+public interface ActorBehaviour {
+   
+    void setMakeOrder(boolean makeOrder);
+
+    void setTakeOrder(boolean takeOrder);
+
+    boolean isMakeOrder();
+
+    boolean isTakeOrder();
+}
